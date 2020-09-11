beb588b1512e34207f5d50be3ff9012d19f0445a
Lars Ivar Hatledal <laht@hials.no>
2020-09-11 21:38:23 +0200

    update

--- threekt-render/src/test/kotlin/info/laht/krender/threekt/TestMesh.kt
+++ threekt-render/src/test/kotlin/info/laht/krender/threekt/TestMesh.kt
@@ -1,8 +1,10 @@
 package info.laht.krender.threekt
 
+import info.laht.krender.mesh.BoxMesh
 import info.laht.krender.mesh.SphereMesh
 import info.laht.threekt.math.Color
 import org.joml.Matrix4f
+import org.joml.Vector3f
 import java.io.File
 
 fun main() {
@@ -13,6 +15,10 @@ fun main() {
         createMesh(SphereMesh(5f)).apply {
             setColor(Color.red)
             setWireframe(true)
+        }
+        createMesh(BoxMesh()).apply {
+            setColor(Color.blue)
+            setTranslate(Vector3f().setComponent(0, 5f))
         }
 
         val objFile = File(javaClass.classLoader.getResource("obj/female02/female02.obj")!!.file)
